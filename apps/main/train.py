9c67625d4411ba3f6f59ceadf30c1ed5f88010ab
Yang Zhou <stevenzhou0816100@gmail.com>
2025-06-26 14:19:30 -0700

    running

--- apps/main/train.py
+++ apps/main/train.py
@@ -251,7 +251,9 @@ def train(args: TrainArgs):
         torch.manual_seed(args.seed)
         logger.info("Building model")
 
-        # Initializing Model in meta device allows us to initialize models much bigger than 1 gpu's memory
+        # Initializing Model in meta device allows us to initialize models much bigger than 1 gpu's memory 
+        print(colored("args.model {}".format(args.model), "green")) 
+        exit(0) 
         with torch.device("meta"):
             model = LMTransformer(args.model)
         logger.info("Model is built !")
@@ -278,7 +280,7 @@ def train(args: TrainArgs):
 
         if args.checkpoint.init_ckpt_path:
             logger.info(f"Loading initial model from {args.checkpoint.init_ckpt_path}")
-            load_from_checkpoint(args.checkpoint.init_ckpt_path, model, model_key="model") # Put model_key="" if its directly the model checkpoint
+            load_from_checkpoint(args.checkpoint.init_ckpt_path, model, model_key="model") # Put model_key="" if its directly the model checkpoint 
             model.rope_embeddings.reset_parameters() # For RoPe initialization since it's a buffer it might not be loaded
         else:
             with torch.random.fork_rng(devices=[torch.cuda.current_device()]):
