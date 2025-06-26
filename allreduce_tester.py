9197a9e28b8c6f764fec5ed531f533e54c1fb40b
Yang Zhou <stevenzhou0816100@gmail.com>
2025-06-25 21:06:58 -0700

    running

--- allreduce_tester.py
+++ allreduce_tester.py
@@ -2,8 +2,9 @@ import torch
 import torch.distributed as dist
 
 # 1. Initialize the distributed environment
-dist.init_process_group(backend="nccl") # or "gloo", "mpi"
+dist.init_process_group(backend="nccl") # or "gloo", "mpi" 
 
 # 2. Perform allreduce on the default process group (WORLD)
-tensor = torch.tensor([1.0])
+tensor = torch.tensor([1.0], device="cuda:{}".format(dist.get_rank())) 
 dist.all_reduce(tensor) 
+print(tensor) 
