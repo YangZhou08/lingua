28e268bcfc0b35ea230fc454984e26408eade686
Yang Zhou <stevenzhou0816100@gmail.com>
2025-07-02 02:24:38 -0700

    running

--- setup/data_preprocessing.sh
+++ setup/data_preprocessing.sh
@@ -1,5 +1,11 @@
+# python download_prepare_hf_data.py \
+#  megamath_pro_web 64 \
+#  --data_dir ./data \
+#  --seed 42 \
+#  --nchunks 8 
+
 python download_prepare_hf_data.py \
- megamath_pro_web 64 \
+ openr1_220k 64 \
  --data_dir ./data \
  --seed 42 \
  --nchunks 8 
