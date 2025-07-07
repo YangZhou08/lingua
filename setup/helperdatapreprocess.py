4edd54dc8a01b51f13f3abd730936e57cda14236
Yang Zhou <stevenzhou0816100@gmail.com>
2025-07-07 02:54:58 -0700

    running

---
+++ setup/helperdatapreprocess.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset 
+
+def generate_textfield(example): 
+    inputtext = "Question: {}\nAnswer: {}".format(example.pop("problem"), example.pop("generations")[0]) 
+    return {"text": inputtext} 
+
+def preprocess_data(datasetpath, outputpath): 
+    dataset = load_dataset(datasetpath, "all", split = "train") 
+    dataset = dataset.map(generate_textfield) 
+    dataset.to_parquet(outputpath) 
+
+if __name__ == "__main__": 
+    datasetpath = "fsx-storygen/jwzhao/yangzho6/lingua/setup/data/openr1_220k" 
+    outputpath = "fsx-storygen/jwzhao/yangzho6/lingua/setup/data/openr1_220k_processed"  
+    preprocess_data(datasetpath, outputpath) 
