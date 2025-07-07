614158c89e22789c64fdf07c2230eff6c6e2ef5e
Yang Zhou <stevenzhou0816100@gmail.com>
2025-07-07 16:04:25 -0700

    running

--- trainscri.sh
+++ trainscri.sh
@@ -6,6 +6,6 @@ huggingface-cli login --token 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
 # python -m lingua.stool script=apps.main.train config=apps/main/configs/debug.yaml nodes=1 partition=<partition> 
 # if you want to launch locally you can use torchrun 
 
-torchrun --nproc-per-node 8 -m apps.main.train config=apps/main/configs/webonly.yaml 
+torchrun --nproc-per-node 8 -m apps.main.train config=apps/main/configs/debug.yaml 
 # or you can also launch on 1 GPU
 # python -m apps.main.train config=apps/main/configs/debug.yaml 
