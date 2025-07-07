b646f92d03781c848ae9634543a51c11a573592e
Yang Zhou <stevenzhou0816100@gmail.com>
2025-07-07 16:16:01 -0700

    running

--- trainscri.sh
+++ trainscri.sh
@@ -2,6 +2,7 @@
 export LD_PRELOAD=/usr/local/cuda-12.<version>/lib/libnccl.so 
 export LD_LIBRARY_PATH=/opt/aws-ofi-nccl/lib/:$LD_LIBRARY_PATH 
 huggingface-cli login --token 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 
+wandb login --relogin fbb26fc8718b8e58d743b5cdcabaa2396656f773 
 
 # python -m lingua.stool script=apps.main.train config=apps/main/configs/debug.yaml nodes=1 partition=<partition> 
 # if you want to launch locally you can use torchrun 
