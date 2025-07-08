4d7a6a64b585dda94081d8c3572b26b455a4facc
Yang Zhou <stevenzhou0816100@gmail.com>
2025-07-07 18:21:12 -0700

    running

--- setup/hfckpt.sh
+++ setup/hfckpt.sh
@@ -2,4 +2,7 @@ inputpath="/fsx-storygen/jwzhao/yangzho6/lingua/checkpoints/debug/checkpoints/00
 cp /fsx-storygen/jwzhao/yangzho6/lingua/checkpoints/llama3b0100/consolidated/tokenizer.model $inputpath 
 echo "Copied tokenizer.model to $inputpath" 
 
+echo "here is the ls" 
+ls -l $inputpath 
+
 python convert_consolidate_hf.py --input_dir $inputpath --llama_version 3.2 --num_shards 1 
