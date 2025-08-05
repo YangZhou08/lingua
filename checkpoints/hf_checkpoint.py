04ea415e7fe78d38523534ea2bcbf3006345dd33
Yang Zhou <stevenzhou0816100@gmail.com>
2025-08-05 03:29:43 -0700

    running

--- checkpoints/hf_checkpoint.py
+++ checkpoints/hf_checkpoint.py
@@ -1,5 +1,5 @@
-CONSOLIDATED_CKPT_PATH = "/data/users/yangzho6/lingua/checkpoints/Llama3.23B/consolidated.00.pth" 
-DCP_DIR_PATH = "/data/users/yangzho6/lingua/checkpoints/llama-3.2-3b-dcp" 
+CONSOLIDATED_CKPT_PATH = "/fsx-storygen/jwzhao/yangzho6/lingua/checkpoints/Llama-3.2-3B/original/consolidated.00.pth" 
+DCP_DIR_PATH = "/fsx-storygen/jwzhao/yangzho6/lingua/checkpoints/debug/checkpoints/llama-3.2-3b-dcp" 
 
 from torch.distributed.checkpoint.format_utils import torch_save_to_dcp
 torch_save_to_dcp(CONSOLIDATED_CKPT_PATH, DCP_DIR_PATH) 
